4c0fb3f01e1736c018d58392981a0bd24c387449
Ruben Torres <rubenbebe.145.rt@gmail.com>
2026-03-11 18:47:51 -0600

    xdxd

--- backend/src/services/service.connection.js
+++ backend/src/services/service.connection.js
@@ -2,7 +2,7 @@
 import { Sequelize } from 'sequelize';
 import config from '../config.js';
 
-/* const sequelize = new Sequelize(config.postgres_database_url,{
+const sequelize = new Sequelize(config.postgres_database_url,{
     dialect: 'postgres',
     logging: false,
     pool: {
@@ -12,18 +12,6 @@ import config from '../config.js';
         idle: 10000
         }
     }
-); */
-
-const sequelize = new Sequelize(config.postgres_db, config.postgres_user, config.postgres_password, {
-    host: config.postgres_host,
-    dialect: 'postgres',
-    logging: false,
-    pool: {
-        max: 5,
-        min: 0,
-        acquire: 30000,
-        idle: 10000
-    }
-}); 
+);
 
 export default sequelize;
